9d73d4013f659188b8998424d8dc15911a5aacc4
christinebahati <christinebahati6@gmail.com>
2024-07-26 14:03:26 +0300

    first commit

---
+++ LibararyManagement2/Form1.Designer.vb
@@ -0,0 +1,214 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Title = New Label()
+        Author = New Label()
+        YearPublished = New Label()
+        genrte = New Label()
+        txtTitle = New TextBox()
+        txtAuthor = New TextBox()
+        txtYearPublished = New TextBox()
+        txtGenre = New TextBox()
+        btnAdd = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnClear = New Button()
+        DataGridView1 = New DataGridView()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Title
+        ' 
+        Title.AutoSize = True
+        Title.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Title.Location = New Point(86, 75)
+        Title.Margin = New Padding(4, 0, 4, 0)
+        Title.Name = "Title"
+        Title.Size = New Size(42, 23)
+        Title.TabIndex = 0
+        Title.Text = "Title"
+        ' 
+        ' Author
+        ' 
+        Author.AutoSize = True
+        Author.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Author.Location = New Point(86, 128)
+        Author.Margin = New Padding(4, 0, 4, 0)
+        Author.Name = "Author"
+        Author.Size = New Size(63, 23)
+        Author.TabIndex = 1
+        Author.Text = "Author"
+        ' 
+        ' YearPublished
+        ' 
+        YearPublished.AutoSize = True
+        YearPublished.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        YearPublished.Location = New Point(86, 174)
+        YearPublished.Margin = New Padding(4, 0, 4, 0)
+        YearPublished.Name = "YearPublished"
+        YearPublished.Size = New Size(116, 23)
+        YearPublished.TabIndex = 2
+        YearPublished.Text = "YearPublished"
+        ' 
+        ' genrte
+        ' 
+        genrte.AutoSize = True
+        genrte.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        genrte.Location = New Point(86, 228)
+        genrte.Margin = New Padding(4, 0, 4, 0)
+        genrte.Name = "genrte"
+        genrte.Size = New Size(56, 23)
+        genrte.TabIndex = 3
+        genrte.Text = "Genre"
+        ' 
+        ' txtTitle
+        ' 
+        txtTitle.Location = New Point(294, 71)
+        txtTitle.Margin = New Padding(4, 3, 4, 3)
+        txtTitle.Name = "txtTitle"
+        txtTitle.Size = New Size(155, 30)
+        txtTitle.TabIndex = 4
+        ' 
+        ' txtAuthor
+        ' 
+        txtAuthor.Location = New Point(294, 128)
+        txtAuthor.Margin = New Padding(4, 3, 4, 3)
+        txtAuthor.Name = "txtAuthor"
+        txtAuthor.Size = New Size(155, 30)
+        txtAuthor.TabIndex = 5
+        ' 
+        ' txtYearPublished
+        ' 
+        txtYearPublished.Location = New Point(294, 174)
+        txtYearPublished.Margin = New Padding(4, 3, 4, 3)
+        txtYearPublished.Name = "txtYearPublished"
+        txtYearPublished.Size = New Size(155, 30)
+        txtYearPublished.TabIndex = 6
+        ' 
+        ' txtGenre
+        ' 
+        txtGenre.Location = New Point(294, 224)
+        txtGenre.Margin = New Padding(4, 3, 4, 3)
+        txtGenre.Name = "txtGenre"
+        txtGenre.Size = New Size(155, 30)
+        txtGenre.TabIndex = 7
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(536, 75)
+        btnAdd.Margin = New Padding(4, 3, 4, 3)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(118, 33)
+        btnAdd.TabIndex = 8
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnUpdate.Location = New Point(536, 118)
+        btnUpdate.Margin = New Padding(4, 3, 4, 3)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(118, 39)
+        btnUpdate.TabIndex = 9
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnDelete.Location = New Point(536, 174)
+        btnDelete.Margin = New Padding(4, 3, 4, 3)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(118, 33)
+        btnDelete.TabIndex = 10
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Segoe UI Semibold", 10F, FontStyle.Bold Or FontStyle.Italic)
+        btnClear.Location = New Point(536, 228)
+        btnClear.Margin = New Padding(4, 3, 4, 3)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(118, 33)
+        btnClear.TabIndex = 11
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.BackgroundColor = SystemColors.ButtonHighlight
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(206, 290)
+        DataGridView1.Margin = New Padding(4, 3, 4, 3)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.Size = New Size(691, 216)
+        DataGridView1.TabIndex = 12
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 23F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.GradientActiveCaption
+        ClientSize = New Size(1000, 518)
+        Controls.Add(DataGridView1)
+        Controls.Add(btnClear)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnAdd)
+        Controls.Add(txtGenre)
+        Controls.Add(txtYearPublished)
+        Controls.Add(txtAuthor)
+        Controls.Add(txtTitle)
+        Controls.Add(genrte)
+        Controls.Add(YearPublished)
+        Controls.Add(Author)
+        Controls.Add(Title)
+        Font = New Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Title As Label
+    Friend WithEvents Author As Label
+    Friend WithEvents YearPublished As Label
+    Friend WithEvents genrte As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtYearPublished As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents DataGridView1 As DataGridView
+
+End Class
